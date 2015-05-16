74e3434d1cfecb2a52e84b3bfb61e7a0a9d83f9d
Nick Cameron <ncameron@mozilla.com>
2015-05-16 18:21:28 +1200

    timing scripts

---
+++ process.sh
@@ -0,0 +1,34 @@
+TIMES_DIR=/root/times
+SCRIPTS_DIR=/root/times-scripts
+
+START=$(pwd)
+export RUSTC="$RUSTC -Ztime-passes"
+export CARGO_BUILD="cargo rustc -- -Ztime-passes"
+export PATH=$RUSTC_DIR/bin:$PATH
+
+for dir in *; do
+    if [[ -d $dir ]]; then
+        echo "Processing $dir"
+        cd $dir
+        for i in 0 1 2 3 4 5
+        do
+            rustc --version >$TIMES_DIR/raw/$dir--$DATE--$i.log
+            echo "rustc: ./$dir" >>$TIMES_DIR/raw/$dir--$DATE--$i.log
+            make >>$TIMES_DIR/raw/$dir--$DATE--$i.log
+            echo "done" >>$TIMES_DIR/raw/$dir--$DATE--$i.log
+
+            make touch >/dev/null
+        done
+        make clean >/dev/null
+
+        cd $TIMES_DIR
+        python $SCRIPTS_DIR/process.py $dir $DATE 6
+        for i in 0 1 2 3 4 5
+        do
+            git add raw/$dir--$DATE--$i.log
+            git add processed/$dir--$DATE--$i.json
+        done
+
+        cd $START
+    fi
+done
---
+++ time.sh
@@ -0,0 +1,47 @@
+# Pulls current rust and benchmarks it.
+
+TIMES_DIR=/root/times
+BENCH_DIR=/root/benchmarks
+SCRIPTS_DIR=/root/times-scripts
+
+export DATE=$(date +%F_%H-%M-%S)
+START=$(pwd)
+
+echo "pulling master"
+git checkout master
+git pull origin master
+
+echo "building"
+
+./configure
+make rustc-stage1 -j8
+
+export RUSTFLAGS_STAGE2=-Ztime-passes
+
+for i in 0 1 2
+do
+    echo "building, round $i"
+    git show HEAD -s >$TIMES_DIR/raw/rustc--$DATE--$i.log
+    touch src/librustc_trans/trans/mod.rs
+    make >>$TIMES_DIR/raw/rustc--$DATE--$i.log
+done
+
+echo "processing data"
+cd $TIMES_DIR
+python $SCRIPTS_DIR/process.py rustc $DATE 3
+for i in 0 1 2
+do
+    git add raw/rustc--$DATE--$i.log
+    git add processed/rustc--$DATE--$i.json
+done
+
+echo "benchmarks"
+export RUSTC=$START/x86_64-unknown-linux-gnu/stage2/bin/rustc
+export LD_LIBRARY_PATH=$START/x86_64-unknown-linux-gnu/stage2/lib
+cd $BENCH_DIR
+./process.sh
+
+echo "committing"
+cd $TIMES_DIR
+git commit -m "Added data for $DATE"
+git push origin master
